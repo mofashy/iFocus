355236ba5728c09cdcc000ffa1471385db4231f4
mofashy <macosx@macdemacbook-pro.local>
2016-03-28 14:57:20 +0800

    Initial Commit

---
+++ iFocus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iFocus
+//
+//  Created by Mac os x on 16/3/28.
+//  Copyright © 2016年 YCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
